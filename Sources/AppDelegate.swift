98b6e15bbc91bd277e0e90e81a12f583c0d48bb7
Luna Graysen <75589876+they-them@users.noreply.github.com>
2021-01-18 08:54:01 -0500

    Add Xcode project
    #1

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,16 @@
+// Created by Luna Graysen on 2021-01-18.
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: UIApplicationDelegate Lifecycle
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
