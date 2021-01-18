053f408d8c1e94760f973a25e6050c140976f166
Luna Graysen <75589876+they-them@users.noreply.github.com>
2021-01-18 08:53:33 -0500

    Add Xcode project

---
+++ Sources/SceneDelegate.swift
@@ -0,0 +1,20 @@
+// Created by Luna Graysen on 2021-01-18.
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    // MARK: Properties
+    var window: UIWindow?
+
+    // MARK: UIWindowSceneDelegate Lifecycle
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else {
+            return
+        }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UIViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
